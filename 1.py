7a7494b08b0e4b4756b0f7118bf7d16ab1e5ddfe
nstsi <42932414+nstsi@users.noreply.github.com>
2020-10-06 14:52:31 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,114 @@
+import urllib.request
+import re
+import pymorphy2
+morph = pymorphy2.MorphAnalyzer()
+from collections import Counter
+import textdistance
+
+
+def parcer(url):
+    page = urllib.request.urlopen(url)
+    text = page.read().decode('utf-8')
+    otzyv = re.findall('ratingValue\">(\d+).+\n.+\n.+\n.+\n.+\n.+\n.+\n.+\n.+\n.+\n.+\n.+\n.+reviewBody\".(.+)<\/p>',
+                       text)
+    return otzyv
+
+
+urls = ['https://www.wildberries.ru/catalog/13546428/otzyvy', 'https://www.wildberries.ru/catalog/12053381/otzyvy',
+        'https://www.wildberries.ru/catalog/13301512/otzyvy', 'https://www.wildberries.ru/catalog/12370161/otzyvy',
+        'https://www.wildberries.ru/catalog/14180209/otzyvy']
+reviews_good = []
+reviews_bad = []
+good_reviews = ''
+bad_reviews = ''
+for i in range(5):
+    url = urls[i]
+    otzyv = parcer(url)
+    for k in range(len(otzyv)):
+        if int(otzyv[k][0]) < 3:
+            reviews_bad.append(otzyv[k][1])
+        elif int(otzyv[k][0]) > 3:
+            reviews_good.append(otzyv[k][1])
+        else:
+            pass
+
+for l in range(len(reviews_bad)):  # это чтобы положительных и отрицательных было поровну
+    good_reviews = good_reviews + ' ' + reviews_good[l]
+    bad_reviews = bad_reviews + ' ' + reviews_bad[l]
+
+good_reviews = re.sub('[₽#&-;\*\(\(,.!?\d]', ' ', good_reviews)  # чистим и токенизируем
+good_reviews = re.sub('&#xA;', '', good_reviews)
+good_reviews = re.sub('&quot;', '', good_reviews)
+good_reviews = re.sub('[a-zA-Z]', '', good_reviews)
+good_reviews = re.sub('\s\s+', ' ', good_reviews)
+# good_reviews = re.sub(r'([а-я])([А-Я])', r'\1 \2', good_reviews)
+good_data = good_reviews.split()
+
+bad_reviews = re.sub('[₽#&-;\*\(\(,.!?\d]', ' ', bad_reviews)  # чистим и токенизируем
+bad_reviews = re.sub('&#xA;', '', bad_reviews)
+bad_reviews = re.sub('&quot;', '', bad_reviews)
+bad_reviews = re.sub('[a-zA-Z]', '', bad_reviews)
+# bad_reviews = re.sub(r'([а-я])([А-Я])', r'\1 \2', bad_reviews)
+bad_reviews = re.sub('\s\s+', ' ', bad_reviews)
+bad_data = bad_reviews.split()
+
+for c in range(len(bad_data)):  # приводим к одному регистру
+    bad_data[c] = bad_data[c].lower()
+for d in range(len(good_data)):
+    good_data[d] = good_data[d].lower()
+
+for e in range(len(bad_data)):  # приводим к начальной форме
+    bad_data[e] = str(morph.parse(bad_data[e]))
+    bad_data[e] = re.findall('normal_form=\'([а-я]+)', bad_data[e])[0]
+for f in range(len(good_data)):
+    good_data[f] = str(morph.parse(good_data[f]))
+    good_data[f] = re.findall('normal_form=\'([а-я]+)', good_data[f])[0]
+
+bad_data = list(dict.fromkeys(bad_data))  # убираем повторения
+good_data = list(dict.fromkeys(good_data))
+
+def word_count(str):
+    counts = dict()
+    words = str.split()
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+    return counts
+print( word_count('the quick brown fox jumps over the lazy dog.'))
+
+
+for x in bad_data:  # оставляем только уникальные слова — такие, каких нет в другом множестве
+    for y in good_data:
+        if x == y:
+            bad_data.remove(x)
+            good_data.remove(y)
+
+check_otzyv = parcer('https://www.wildberries.ru/catalog/14180209/otzyvy')  # берем отзывы для проверки
+
+for m in range(len(check_otzyv)):
+    check_review = str(check_otzyv[m][1])
+    check_review = re.sub('[₽#&-;\*\(\(,.!?\d]', ' ', check_review)  # чистим и токенизируем
+    check_review = re.sub('\[\]', '', check_review)
+    check_review = re.sub('&#xA;', '', check_review)
+    check_review = re.sub('&quot;', '', check_review)
+    check_review = re.sub('[a-zA-Z]', '', check_review)
+    check_review = re.sub('\s\s+', ' ', check_review)
+    check_review = re.sub(r'([а-я])([А-Я])', r'\1 \2', check_review)
+    check_review = check_review.split()
+    for p in range(len(check_review)):  # приводим к одному регистру
+        check_review[p] = check_review[p].lower()
+    for r in range(len(check_review)):  # приводим к начальной форме
+        check_review[r] = str(morph.parse(check_review[r]))
+        check_review[r] = re.findall('normal_form=\'([а-я]+)', check_review[r])[0]
+        #get_closest_match_with_metric(check_review[r], X, vec, textdistance.hamming)
+
+
+def get_closest_match_with_metric(text, X, vec, metric=textdistance.levenshtein):
+    similarities = Counter()
+    lookup = get_closest_match_vec(text, X, vec, TOPN=3)
+    for word in lookup:
+        similarities[word] = metric.normalized_similarity(text, word)
+    return similarities.most_common(1)[0]
+
